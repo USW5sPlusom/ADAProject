d0cf2e3355477345af1cb3bf65d703af172c6883
Aleksey <vaznoe2024@mail.ru>
2025-09-25 11:15:14 +0300

    v.2 Добавлена и зарегестрирована модель для записи в БД

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, default = '')
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        pass
+    
+    def __str__():
+        pass
